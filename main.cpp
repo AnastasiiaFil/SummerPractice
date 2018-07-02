4662114faf84b004c9dd2822ddb6f7cbb25eeeed
Emmanuil Prokopov <emmanuilprokopov@gmail.com>
2018-07-02 15:11:18 +0300

    Add file with code

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <cmath>
 using namespace std;
 
-double function(double x)///////////////ghrthnerre6e5jhn65e
+double function(double x) // Объявляем функцию
 {
 	return  ((x*x*x*x) + 8 * (x*x*x) - 6 * (x*x) - 72 * x + 90);
 }
 int main()
 {
-	int iterations = 0;
-	double a = 1.5, b=2.0, x_m = (a+b)/2;
+	int iterations = 0; // Счётчик итераций
+	double a = 1.5, b=2.0, x_m = (a+b)/2; // a и b - начальная и конечная точки, x_m - середина отрезка
 	double y, z, f1, f2, f3, eps = 0.0001;
 	double res_x, L2;
 
@@ -45,8 +45,8 @@ int main()
     }
     while( abs( L2 ) > eps );
     res_x = (a + b) / 2;
-    cout <<  fixed << setprecision(33) << res_x;
-    cout <<  fixed << setprecision(33) << function(res_x);
+    cout <<  fixed << setprecision(33) << res_x; // Выводим значение точки минимума с кол-вом знаков после запятой: 33
+    cout <<  fixed << setprecision(33) << function(res_x); // Выводим значение функции в точке минимума
     cout <<  iterations;
     return 0;
 }
